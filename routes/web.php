482288925caef5286fd5da49a611491f6e52c065
SKSritharan <sk.sritharan75@gmail.com>
2024-11-10 22:34:47 +0530

    Fixed the mail view

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (){
     return view('welcome');
 });
-
-Route::get('/test', function (){
-    return view('mail.user-verified', [
-        'user' => App\Models\User::first(),
-    ]);
-});
